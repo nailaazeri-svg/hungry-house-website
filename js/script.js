eddbd244bacdc4030d4f005ae83b4812d711ff1c
nailaazeri-svg <naila.azeri@gmail.com>
2026-03-04 22:37:34 +0100

    Fix images folder and update files

--- js/script.js
+++ js/script.js
@@ -2,43 +2,50 @@ const searchForm = document.querySelector(".search-form");
 const cartItem = document.querySelector(".cart-items-container");
 const navbar = document.querySelector(".navbar");
 
-//! buttons
+// Buttons
 const searchBtn = document.querySelector("#search-btn");
 const cartBtn = document.querySelector("#cart-btn");
 const menuBtn = document.querySelector("#menu-btn");
 
-searchBtn.addEventListener("click", function(){
-   searchForm.classList.toggle("active");
-   document.addEventListener("click", function(e){
-    if (
-        !e.composedPath().includes(searchBtn) && 
-        !e.composedPath().includes(searchForm)
-    ) {
-     searchForm.classList.remove("active");    
-    }
-   });
-}); 
-
-cartBtn.addEventListener("click", function(){
-   cartItem.classList.toggle("active");
-   document.addEventListener("click", function(e){
-    if (
-        !e.composedPath().includes(cartBtn) && 
-        !e.composedPath().includes(cartItem)
-    ) {
-     cartItem.classList.remove("active");    
-    }
-   });
+// Toggle actions
+searchBtn.addEventListener("click", () => {
+  searchForm.classList.toggle("active");
+  cartItem.classList.remove("active");
+  navbar.classList.remove("active");
 });
 
-menuBtn.addEventListener("click", function(){
-   navbar.classList.toggle("active");
-   document.addEventListener("click", function(e){
-    if (
-        !e.composedPath().includes(menuBtn) && 
-        !e.composedPath().includes(navbar)
-    ) {
-     navbar.classList.remove("active");    
-    }
-   });
+cartBtn.addEventListener("click", () => {
+  cartItem.classList.toggle("active");
+  searchForm.classList.remove("active");
+  navbar.classList.remove("active");
+});
+
+menuBtn.addEventListener("click", () => {
+  navbar.classList.toggle("active");
+  searchForm.classList.remove("active");
+  cartItem.classList.remove("active");
+});
+
+// Outside click control
+document.addEventListener("click", (e) => {
+  if (
+    !e.composedPath().includes(searchBtn) &&
+    !e.composedPath().includes(searchForm)
+  ) {
+    searchForm.classList.remove("active");
+  }
+
+  if (
+    !e.composedPath().includes(cartBtn) &&
+    !e.composedPath().includes(cartItem)
+  ) {
+    cartItem.classList.remove("active");
+  }
+
+  if (
+    !e.composedPath().includes(menuBtn) &&
+    !e.composedPath().includes(navbar)
+  ) {
+    navbar.classList.remove("active");
+  }
 });
